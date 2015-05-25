e527ffb5a521ed42c5b22f167b07cbcd6bf0fd1c
stephendiadamo <stephen.diadamo@gmail.com>
2015-05-25 16:01:48 -0400

    Added navigation labels

--- app/src/main/java/com/s_diadamo/readlist/MainActivity.java
+++ app/src/main/java/com/s_diadamo/readlist/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends ActionBarActivity
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
-    private CharSequence mTitle;
+    private CharSequence mTitle = getString(R.string.app_name);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,17 +57,7 @@ public class MainActivity extends ActionBarActivity
     }
 
     public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
+        return;
     }
 
     public void restoreActionBar() {
--- app/src/main/java/com/s_diadamo/readlist/NavigationDrawerFragment.java
+++ app/src/main/java/com/s_diadamo/readlist/NavigationDrawerFragment.java
@@ -102,9 +102,10 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.navigation_drawer_reading),
+                        getString(R.string.navigation_drawer_bookshelves),
+                        getString(R.string.navigation_drawer_goals),
+                        getString(R.string.navigation_drawer_statistics)
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
