4e7bedc356c49ff0643f8b0b4977bc422cd36c08
stephendiadamo <stephen.diadamo@gmail.com>
2015-06-05 19:33:09 -0400

    Need to delete API file…

--- app/src/main/java/com/s_diadamo/readlist/API.java
+++ app/src/main/java/com/s_diadamo/readlist/API.java
@@ -5,4 +5,7 @@ package com.s_diadamo.readlist;
  */
 public class API {
     private static final String GOOGLE_BOOKS_API_KEY = "AIzaSyBiDdDU7WZ7gw6iU12HSfHTv79F09lWP70";
+    public static String getGoogleBooksApiKey(){
+        return GOOGLE_BOOKS_API_KEY;
+    }
 }
