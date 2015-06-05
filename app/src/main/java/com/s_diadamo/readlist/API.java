65a9608c8a07cd57e0c1a31fbce2ca5e6d03a3fb
Stephen Diadamo <stephen.diadamo@gmail.com>
2015-06-05 19:30:52 -0400

    Delete API.java

--- app/src/main/java/com/s_diadamo/readlist/API.java
+++
@@ -1,8 +0,0 @@
-package com.s_diadamo.readlist;
-
-/**
- * Created by s-diadamo on 15-05-27.
- */
-public class API {
-    private static final String GOOGLE_BOOKS_API_KEY = "AIzaSyBiDdDU7WZ7gw6iU12HSfHTv79F09lWP70";
-}
