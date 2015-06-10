edc1ae138fe50a217e27c9e205be3d95e6519233
stephendiadamo <stephen.diadamo@gmail.com>
2015-06-10 19:30:52 -0400

    Added a default shelf on first launch

--- app/src/main/java/com/s_diadamo/readlist/MainActivity.java
+++ app/src/main/java/com/s_diadamo/readlist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.s_diadamo.readlist;
 
-import android.support.v7.app.ActionBar;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
@@ -13,11 +14,15 @@ import com.s_diadamo.readlist.book.BookFragment;
 import com.s_diadamo.readlist.goal.GoalsFragment;
 import com.s_diadamo.readlist.lazylist.ImageLoader;
 import com.s_diadamo.readlist.navigationDrawer.NavigationDrawerFragment;
+import com.s_diadamo.readlist.shelf.Shelf;
+import com.s_diadamo.readlist.shelf.ShelfOperations;
 import com.s_diadamo.readlist.statistics.StatisticsFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
+
+    private static final String CREATED_SHELF = "CREATED_SHELF";
     private NavigationDrawerFragment mNavigationDrawerFragment;
     private CharSequence mTitle;
     public static ImageLoader imageLoader;
@@ -36,6 +41,16 @@ public class MainActivity extends AppCompatActivity
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
         imageLoader = new ImageLoader(this);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!prefs.getBoolean(CREATED_SHELF, false)) {
+            Shelf defaultShelf = new Shelf(Shelf.DEFAULT_SHELF_ID, "All Books", Shelf.DEFAULT_COLOR);
+            (new ShelfOperations(this)).addShelf(defaultShelf);
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(CREATED_SHELF, true);
+            editor.apply();
+        }
     }
 
     @Override
@@ -47,36 +62,25 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         Fragment objFragment = null;
-        String tag = "";
 
         switch (position) {
             case 0:
                 objFragment = new BookFragment();
-                tag = "BOOK";
                 break;
             case 1:
                 break;
             case 2:
                 objFragment = new GoalsFragment();
-                tag = "GOALS";
                 break;
             case 3:
                 objFragment = new StatisticsFragment();
-                tag = "STATS";
                 break;
         }
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, objFragment, tag)
+                .replace(R.id.container, objFragment)
                 .commit();
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
     }
 
     @Override
