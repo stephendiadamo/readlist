e9bd573e314845e3e0e07271f841616842408f98
stephendiadamo <stephen.diadamo@gmail.com>
2015-07-06 17:39:12 -0400

    Fix

--- app/src/main/java/com/s_diadamo/readlist/general/API.java
+++
@@ -1,9 +0,0 @@
-package com.s_diadamo.readlist.general;
-
-public class API {
-    private static final String GOOGLE_BOOKS_API_KEY = "AIzaSyCymhKCc7BMH3vtPnbLJ9-nXdbA9AZmBSg";
-    public static String getGoogleBooksApiKey() {
-        return GOOGLE_BOOKS_API_KEY;
-    }
-}
-
