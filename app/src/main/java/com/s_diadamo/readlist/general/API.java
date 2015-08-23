dc5719bd4d5dd48b2c2f2f51b2e29360bdd37f5f
stephendiadamo <stephen.diadamo@gmail.com>
2015-08-23 13:43:50 -0400

    Need API file

---
+++ app/src/main/java/com/s_diadamo/readlist/general/API.java
@@ -0,0 +1,13 @@
+package com.s_diadamo.readlist.general;
+
+public class API {
+    private static final String GOOGLE_BOOKS_API_KEY = "AIzaSyAS7nlmwrAdSSYVF4_QybxWQM0FR6Xcbe8";
+    public static final String PARSE_APP_KEY = "qiDNOPTJlWjUtSm7fXQOnDUtS2ueIYMiHQyC7TLr";
+    public static final String PARSE_CLIENT_KEY = "miGNSQUi6hPnUuY7yz7LKctMvW1Mu3Vu1SdQRIxv";
+
+    public static String getGoogleBooksApiKey() {
+        return GOOGLE_BOOKS_API_KEY;
+    }
+
+}
+
